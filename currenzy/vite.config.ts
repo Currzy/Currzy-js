1a15aa5e778f1f8f8d10f015c65e26ce324504fb
VaheSargsyan2005 <w33bv.gl@gmail.com>
2025-09-12 16:53:24 +0400

    add new pakcage

--- currenzy/vite.config.ts
+++ currenzy/vite.config.ts
@@ -1,6 +1,19 @@
 import { defineConfig } from 'vitest/config';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+    plugins: [tsconfigPaths()],
+    build: {
+        lib: {
+            entry: 'src/index.ts',
+            name: 'Currenzy',
+            fileName: (format) => `currenzy.${format}.js`,
+            formats: ['es', 'cjs']
+        },
+        rollupOptions: {
+            external: ['fast-xml-parser', 'ofetch']
+        }
+    },
     test: {
         globals: true,
         environment: 'node',
