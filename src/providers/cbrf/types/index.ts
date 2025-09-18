f625b92e60efd56597d0dcd6f7d79fff45d4db40
VaheSargsyan2005 <w33bv.gl@gmail.com>
2025-09-18 15:46:00 +0400

    fix src

--- src/providers/cbrf/types/index.ts
+++ src/providers/cbrf/types/index.ts
@@ -2,7 +2,7 @@ export * from "./rate";
 export * from "./currency";
 export * from "./api";
 
-import type { Rate, CurrencyCode } from "@src/providers/cbrf/types";
+import type { Rate, CurrencyCode } from "@src/providers/cbrf/types/index";
 
 export interface CbrfCache {
     rates: Record<CurrencyCode, Rate>;
